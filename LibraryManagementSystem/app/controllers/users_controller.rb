55f1353374c05e0f3df5c5a2572eef4a2872f1f2
jatin <ajjatin31@gmail.com>
2026-04-02 22:21:03 +0530

    Initial commit

--- LibraryManagementSystem/app/controllers/users_controller.rb
+++ LibraryManagementSystem/app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :set_user, only: %i[show update destroy]
 
